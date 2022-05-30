cac3f6d238a25deba0d315339f46051adbd4bb0a
series.infodev <series.chikanbanjar@infodevelopers.com.np>
2022-05-30 14:38:43 +0545

    feat(pivottable): create pivot table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import PivotTableUI from "react-pivottable/PivotTableUI";
+import "react-pivottable/pivottable.css";
+
+import TableRenderers from "react-pivottable/TableRenderers";
+import Plot from "react-plotly.js";
+import createPlotlyRenderers from "react-pivottable/PlotlyRenderers";
+
+import dummyData from "./data/dummyData.json";
+
+export interface PivotTableDataProps {
+    pivotData: Record<string, any>[];
+}
+
+const pivotPresets = {
+    cols: ["pizza"],
+    rows: ["employee"],
+    rendererName: "Table",
+    aggregatorName: "Sum",
+    vals: ["total"],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tableData, setTableData] = useState<PivotTableDataProps>({
+        pivotData: [],
+    });
+    const [pivotConfig, setPivotConfig] = useState(pivotPresets);
+
+    useEffect(() => {
+        if (dummyData) {
+            setTableData({
+                ...tableData,
+                ["pivotData"]: dummyData?.data,
+            });
+        }
+    }, []);
+
+    const PlotlyRenderers = createPlotlyRenderers(Plot);
+    return (
+        <div>
+            <PivotTableUI
+                className="table"
+                data={tableData?.pivotData}
+                onChange={(e: any) => {
+                    setTableData(e);
+                }}
+                renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
+                {...pivotConfig}
+                {...tableData}
+            />
+        </div>
+    );
 }
 
 export default App;
